4bb5750f9899dcc63139180d6b2269c98d7fc8c6
MyDevGarden <nilima.raundal@gmail.com>
2024-07-01 11:15:33 +0530

    Handling CORS Policy

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,34 @@ import express from "express";
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookmodel.js";
-import bookRoute from "./Routes/bookroutes.js"
+import bookRoute from "./Routes/bookroutes.js";
+import cors from "cors";
+
 
 const app= new express();
 //middleware to parsing request body
 app.use(express.json());
 
+//middleware to handle routes
+app.use('/books', bookRoute);
 
+//middleware for handling CORS POLICY
+//Option1: Allow all origins with dafault of cors(*)
+//app.use(cors());
+//option 2: Allow Custom Origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 app.get('/', (req,res) =>{
     console.log(req);
     return res.status(234).send("welcome to book store");
 });
-app.use('/books', bookRoute);
+
 
 mongoose
     .connect(mongoDBURL)
