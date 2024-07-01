a209172939b62d874e53583adb3a1e1a18eafcdb
MyDevGarden <nilima.raundal@gmail.com>
2024-07-01 10:43:54 +0530

    Basic CRUD operations

--- backend/index.js
+++ backend/index.js
@@ -44,13 +44,65 @@ app.post('/books', async(req, res) => {
 app.get('/books',async (req,res) => {
     try{
         const books= await Book.find({});
-        res.status(200).json(books);
+        res.status(200).json({
+            count: books.length,
+            data: books
+        });
     }catch(error){
         console.log(error);
         res.status(500).send({message: error.message});
     }
 })
 
+//Route for getting  book by id
+app.get('/books/:id',async (req,res) => {
+    try{
+        const {id} = req.params;
+        const book= await Book.findById(id);
+        res.status(200).json(book);
+    }catch(error){
+        console.log(error);
+        res.status(500).send({message: error.message});
+    }
+})
+
+//Route for updae a book
+app.put('/books/:id', async(req, res)=>{
+    try{
+        if(
+            !req.body.title || 
+            !req.body.author || 
+            !req.body.publishYear){
+            return res.status(400).send({message: "send all required fields: title, author, publishYear"});
+        }
+        const {id} = req.params;
+        const result = await Book.findByIdAndUpdate(id, req.body);
+        if(!result)
+            {
+                return res.status(404).send({message: "Book not found"});
+            }
+            return res.status(200).send({message: "Book updated successfully"});
+    }catch(error){
+        console.log(error);
+        res.status(500).send({message: error.message});
+    }
+});
+
+//route for deleting book
+app.delete('/books/:id', async(req, res)=>{
+    try{
+        const {id} = req.params;
+        const result = await Book.findByIdAndDelete(id, req.body);
+        if(!result)
+            {
+                return res.status(404).send({message: "Book not found"});
+            }
+            return res.status(200).send({message: "Book deleted successfully"});
+    }catch(error){
+        console.log(error);
+        return res.status(500).send({message: error.message});
+    }
+})
 mongoose
     .connect(mongoDBURL)
     .then(()=>{
