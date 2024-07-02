7f63bf71b833eb7e70e2887f5b74c0f3f98e32fc
MyDevGarden <nilima.raundal@gmail.com>
2024-07-02 10:26:40 +0530

    Show book list in React

--- backend/index.js
+++ backend/index.js
@@ -9,21 +9,30 @@ import cors from "cors";
 const app= new express();
 //middleware to parsing request body
 app.use(express.json());
+app.use(cors());
 
 //middleware to handle routes
 app.use('/books', bookRoute);
 
 //middleware for handling CORS POLICY
 //Option1: Allow all origins with dafault of cors(*)
-//app.use(cors());
+
+// const corsOptions = {
+    
+//     origin: '*',
+//     credentials: true,
+//     optionSuccessStatus: 200
+//   }
+//   app.use(cors(corsOptions))
+  
 //option 2: Allow Custom Origins
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:5173',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 app.get('/', (req,res) =>{
     console.log(req);
